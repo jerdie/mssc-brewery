d8ea73d7c9127bed976662b796b1b46c3e2241c9
jerdie <5143814+jerdie@users.noreply.github.com>
2021-11-10 23:40:37 +0800

    fix: add beerid in delete

--- src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/BeerController.java
@@ -38,7 +38,7 @@ public class BeerController {
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/{beerId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteBeer(@PathVariable UUID beerId){
         beerService.deleteBeer(beerId);
